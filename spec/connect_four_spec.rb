67806a216f5296b34fb15ca90629b3815915c781
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-02-08 07:40:31 +0000

    input_to_index added + Code rejig

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -3,20 +3,10 @@ require '../lib/connect-four'
 describe ConnectFour do
   subject(:connectfour) { described_class.new }
 
-  describe '#counter_drop' do
-    context 'when an empty column is chosen' do
-      it 'puts the piece in the lowest position' do
-        expect (connectfour.counter_drop(0, 'O')).to change { connectfour.row_six[0] }.from (' ').to('O')
-      end
-    end
-
-    context 'when a non-empty column is chosen' do
-      before do
-        row_six[3] = 'O'
-      end
-
-      it 'puts the piece at the lowest available position' do
-        expect (connectfour.counter_drop(3, 'O')).to change { connectfour.row_five[3] }.from (' ').to('O')
+  describe 'input_to_index' do
+    context 'when an input is provided' do
+      it 'converts to matching index' do
+        expect (connectfour.input_to_index(4)).to eq(5)
       end
     end
   end
@@ -31,6 +21,24 @@ describe ConnectFour do
     context 'when an invalid number is provided' do
       it 'returns false' do
         expect (connectfour.valid?(9)).to be false
+      end
+    end
+  end
+
+  describe '#counter_drop' do
+    context 'when an empty column is chosen' do
+      it 'puts the piece in the lowest position' do
+        expect (connectfour.counter_drop(0, 'O')).to change { connectfour.row_six[0] }.from (' ').to('O')
+      end
+    end
+
+    context 'when a non-empty column is chosen' do
+      before do
+        row_six[3] = 'O'
+      end
+
+      it 'puts the piece at the lowest available position' do
+        expect (connectfour.counter_drop(3, 'O')).to change { connectfour.row_five[3] }.from (' ').to('O')
       end
     end
   end
