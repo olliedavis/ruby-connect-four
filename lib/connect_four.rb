6548304172d06d30cc043ab19778dd0ae667d1ee
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-02-09 19:09:32 +0000

    valid? method added and passed

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,7 +1,12 @@
-require_relative '/lib/board'
+require_relative 'gameboard'
 
 class ConnectFour
   def initialize
-    @board = Board.new
+    @board = Gameboard.new
   end
+
+  def valid?(int)
+    int >= 1 && int <= 7 ? true : false
+  end
+  
 end
