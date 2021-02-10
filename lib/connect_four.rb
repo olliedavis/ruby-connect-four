7e5e6dff1aa125d76d1318484dc0f9d2be878a58
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-02-10 06:57:50 +0000

    counter position checker added

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -11,7 +11,7 @@ class ConnectFour
     input = gets.chomp.to_i - 1
 
     until valid?(input)
-      puts "Invalid input, please enter the number of the column of where you would like to drop your token"
+      puts 'Invalid input, please enter the number of the column of where you would like to drop your token'
       input = gets.chomp.to_i - 1
     end
     input
@@ -20,5 +20,6 @@ class ConnectFour
   def valid?(int)
     int >= 1 && int <= 7 ? true : false
   end
+
 
 end
--- lib/gameboard.rb
+++ lib/gameboard.rb
@@ -9,7 +9,29 @@ class Gameboard
     @row_three = [' '] * 7
     @row_four = [' '] * 7
     @row_five = [' '] * 7
-    @row_six = [' '] * 7
+    @row_six = [' x'] * 7
+  end
+
+  def counter_drop(column, player)
+    
+  end
+
+  def counter_drop_position(column)
+    case ' '
+    when @row_six[column]
+      @row_six
+    when @row_five[column]
+      @row_five
+    when @row_four[column]
+      @row_four
+    when @row_three[column]
+      @row_three
+    when @row_two[column]
+      @row_two
+    when @row_one[column]
+      @row_one
+    end
   end
 end
+
 
