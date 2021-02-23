aeb7cb6b7d52f7da3f6bc8d34770f72dae00b777
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-02-23 20:16:22 +0000

    load_file refined and fixed

--- lib/load_game.rb
+++ lib/load_game.rb
@@ -1,16 +1,16 @@
 require 'json'
 
 class LoadGame
+  
   def load_file
-    puts 'Which one is the name of your save file?'
-    begin
-      retrieve_files
+    puts 'Which file is the name of your save?'
+    retrieve_files
+    user_save = gets.chomp
+    until File.exist?("../saves/#{user_save}.json")
+      puts 'File not found, try again'
       user_save = gets.chomp
-      save = File.read("../saves/#{user_save}.json")
-    rescue StandardError
-      puts 'File not found. Going back to main menu.'
     end
-    JSON.parse(save)
+    JSON.parse(File.read("../saves/#{user_save}.json"))
   end
 
   def retrieve_files
@@ -19,4 +19,17 @@ class LoadGame
       puts File.basename(save_file, '.json') unless save_file =~ /^..?$/
     end
   end
+end
+
+
+def load_file
+  puts 'Which file is the name of your save?'
+  retrieve_files
+  user_save.gets.chomp
+  until user_save == File.read("../saves/#{user_save}.json")
+    puts 'File not found, try again'
+    load_file
+  end
+  save = File.read("../saves/#{user_save}.json")
+  JSON.parse(save)
 end
