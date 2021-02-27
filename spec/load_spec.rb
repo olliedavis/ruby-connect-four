0c21bacf5b2e19b57dc1ea76e0954a5ec4dc236a
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-02-27 19:00:28 +0000

    beginning of spec files created

---
+++ spec/load_spec.rb
@@ -0,0 +1,11 @@
+require 'rspec'
+describe LoadGame do
+  subject(:load) { described_class.new }
+
+  describe '#load_file' do
+    context 'when function is called' do
+      it 'updates the board with the saved version' do
+      end
+    end
+  end
+end
---
+++ spec/save_spec.rb
@@ -0,0 +1,11 @@
+require 'rspec'
+describe SaveGame do
+  subject(:save) { described_class.new }
+
+  describe '#game_save' do
+    context 'when function is called' do
+      it 'creates a file in the saves folder' do
+      end
+    end
+  end
+end
